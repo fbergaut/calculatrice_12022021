5181e996dff488ebd550475f49482861ff403f51
fbergaut <f.bergaut@gmail.com>
2021-02-22 14:06:11 +0100

    optimisations with my mentor

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const equal = document.querySelector(".equal");
 const result = document.querySelector("#result");
 const clearCalc = document.getElementById("clear");
 
+const btnOps = document.querySelectorAll(".btnOp");
+
 let chosenOp;
 
 function showResult() {
@@ -22,34 +24,64 @@ function showResult() {
     result.textContent = Number(input1.value) * Number(input2.value);
   } else if (chosenOp === "/") {
     result.textContent = Number(input1.value) / Number(input2.value);
-  }
+  } 
+  btnOps.forEach(btn => {
+    btn.classList.remove("btn-active");
+  });
+
 }
 
 // equal.addEventListener("click", function () {
 //   console.log("ça marche !");
 // });
 
-add.addEventListener("click", function () {
-  // operationChosen.innerHTML = "Opération choisie " + add.textContent;
-  chosenOp = add.textContent;
+btnOps.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    btnOps.forEach((btn) => {
+      btn.classList.remove("btn-active");
+    });
+    chosenOp = event.target.textContent;
+    event.target.classList.add("btn-active");
+  });
 });
 
-subtract.addEventListener("click", function () {
-  // operationChosen.innerHTML = subtract.textContent;
-  chosenOp = subtract.textContent;
-});
+// add.addEventListener("click", function () {
+//   // operationChosen.innerHTML = "Opération choisie " + add.textContent;
+//   btnOps.forEach((btn) => {
+//     btn.classList.remove("btn-active");
+//   });
+//   chosenOp = add.textContent;
+//   add.classList.add("btn-active");
+// });
 
-multiply.addEventListener("click", function () {
-  // operationChosen.innerHTML = multiply.textContent;
-  chosenOp = multiply.textContent;
-});
+// subtract.addEventListener("click", function () {
+//   // operationChosen.innerHTML = subtract.textContent;
+//   btnOps.forEach((btn) => {
+//     btn.classList.remove("btn-active");
+//   });
+//   chosenOp = subtract.textContent;
+//   subtract.classList.add("btn-active");
+// });
 
-divide.addEventListener("click", function () {
-  // operationChosen.innerHTML = divide.textContent;
-  chosenOp = divide.textContent;
-});
+// multiply.addEventListener("click", function () {
+//   // operationChosen.innerHTML = multiply.textContent;
+//   btnOps.forEach((btn) => {
+//     btn.classList.remove("btn-active");
+//   });
+//   chosenOp = multiply.textContent;
+//   multiply.classList.add("btn-active");
+// });
 
-clearCalc.addEventListener("click", function() {
+// divide.addEventListener("click", function () {
+//   // operationChosen.innerHTML = divide.textContent;
+//   btnOps.forEach((btn) => {
+//     btn.classList.remove("btn-active");
+//   });
+//   chosenOp = divide.textContent;
+//   divide.classList.add("btn-active");
+// });
+
+clearCalc.addEventListener("click", function () {
   input1.value = "";
   input2.value = "";
   result.textContent = "";
